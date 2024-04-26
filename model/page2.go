00a5c5590376e2dd190a209110fbe78b60ef9174
Hieu1512 <hieunguyenviet2016@.com>
2024-04-26 16:15:36 +0700

    add main.go

--- model/page2.go
+++ model/page2.go
@@ -1,12 +1,20 @@
 package model
 
 import (
+	"time"
+
 	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
+type CustomModel struct {
+	Id       uint `gorm:"primaryKey" json:"id"`
+	CreateAt time.Time
+	UpdateAt time.Time
+	DeleteAt gorm.DeletedAt
+}
 type Page2 struct {
-	gorm.Model
+	CustomModel
 	Label    string
 	Key      string
 	Link     string
